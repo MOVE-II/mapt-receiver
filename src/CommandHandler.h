7a7d631fa13859ba9372c4c220691e05318e4e6c
Jonas Sticha <jonas.sticha@tum.de>
2016-06-22 13:50:35 +0200

    Add initial CommandHandler class

---
+++ src/CommandHandler.h
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2016  Jonas Sticha (jonas.sticha@tum.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef MAPT_RECEIVER_COMMANDHANDLER_H
+#define MAPT_RECEIVER_COMMANDHANDLER_H
+
+#include "Command.h"
+#include "DataHandler.h"
+
+class CommandHandler {
+private:
+    CommandHandler();
+    DataHandler& dataHandler;
+public:
+    CommandHandler(DataHandler dataHandler);
+};
+
+
+#endif //MAPT_RECEIVER_COMMANDHANDLER_H
